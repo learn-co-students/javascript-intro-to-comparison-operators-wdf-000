8399ee16735cb21db4a8b92f72f16b76e0a71da6
vicfriedman <victoria.m.friedman@gmail.com>
2015-12-22 12:33:29 -0500

    solution

--- js/comparison.js
+++ js/comparison.js
@@ -1,1 +1,32 @@
 'use strict';
+
+function testFalseLooseEqualityComparison(){
+  var bar = 0;
+  var foo = 1;  
+  return bar == foo;
+}
+
+function testTrueLooseNonEqualityComparison(){
+  var bar = 0;
+  var foo = 1;  
+  return bar != foo;
+}
+
+function testTrueLooseEqualityComparison(){
+  var foo = 1;  
+  var yoo = "1"
+  return foo == yoo;
+}
+
+
+function testFalseStrictEqualityComparison(){
+  var foo = 1;
+  var yoo = '1';
+  return foo === yoo;
+}
+
+function testTrueStrictEqualityComparison(){
+  var foo = 1;
+  var yoo = '1';
+  return foo === parseInt(yoo);
+}
