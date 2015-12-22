5e03c278c8fb8985eff87ab74fc4e10366498e83
vicfriedman <victoria.m.friedman@gmail.com>
2015-12-22 12:33:12 -0500

    tests

---
+++ spec/comparison_spec.js
@@ -0,0 +1,39 @@
+'use strict';
+describe('Intro to Jasmine and JS basics', function() {
+  
+  describe('#testFalseLooseEqualityComparison', function() {
+    it('should return false because 0 and 1 are not equal in value', function() {
+      expect(testFalseLooseEqualityComparison()).toBe(false);
+    });
+    
+  });
+
+  describe('#testTrueLooseNonEqualityComparison', function() {
+    it('should return true because 0 and 1 are not equal in value', function() {
+      expect(testTrueLooseNonEqualityComparison()).toBe(true);
+    });
+  });
+
+  describe('#testTrueLooseEqualityComparison', function() {
+    it('should return true because "1" and 1 are equal in value', function() {
+      expect(testTrueLooseEqualityComparison()).toBe(true);
+    });
+  });
+
+  describe('#testFalseStrictEqualityComparison', function() {
+    it('should return false because 1 and "1" are not the same datatype', function() {
+      expect(testFalseStrictEqualityComparison()).toBe(false);
+    });
+    
+  });
+
+  describe('#testTrueStrictEqualityComparison', function() {
+    it('should return false because 1 and parseInt("1") are the same datatype', function() {
+      expect(testTrueStrictEqualityComparison().toBe(true);
+    });
+    
+  });
+
+
+ 
+});
