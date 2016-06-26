cca3af73c187530fb40f1764955d79a5958922a4
uu <giladbarkan@live.com>
2016-06-26 01:35:41 -0400

    Done.

--- js/comparison.js
+++ js/comparison.js
@@ -1,1 +1,36 @@
 'use strict';
+
+function testFalseLooseEqualityComparison(){
+  var a = 0, 
+      b = 1;
+
+  return a == b;
+}
+
+function testTrueLooseNonEqualityComparison(){
+  var a = 0,
+      b = 1;
+
+  return a != b;
+}
+
+function testTrueLooseEqualityComparison(){
+  var a = 1,
+      b = 1
+
+  return a == b;
+}
+
+function testFalseStrictEqualityComparison(){
+  var a = 1,
+      b = "1"
+
+  return a === b;
+}
+
+function testTrueStrictEqualityComparison(){
+  var a = 1,
+      b = "1"
+
+  return a === parseInt(b);
+}
