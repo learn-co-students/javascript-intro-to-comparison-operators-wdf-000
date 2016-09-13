e382ed554f2d5338dbea4db30e1393c32b8d195d
Margaret <eutopia@gmail.com>
2016-09-13 14:35:05 -0400

    Done.

--- js/comparison.js
+++ js/comparison.js
@@ -1,1 +1,21 @@
 'use strict';
+
+function testFalseLooseEqualityComparison() {
+	return 0 == 1;
+}
+
+function testTrueLooseNonEqualityComparison() {
+	return 0 != 1;
+}
+
+function testTrueLooseEqualityComparison() {
+	return "1" == 1;
+}
+
+function testFalseStrictEqualityComparison() {
+	return "1" === 1;
+}
+
+function testTrueStrictEqualityComparison() {
+	return 1 === parseInt('1');
+}
