8b4315385225ce54bde25e58d2943549f6727ff5
satub <satu.barnhill@gmail.com>
2016-10-10 12:14:30 -0400

    Done.

--- js/comparison.js
+++ js/comparison.js
@@ -1,1 +1,38 @@
 'use strict';
+var firstVar;
+var secondVar;
+
+function testFalseLooseEqualityComparison(){
+  firstVar = 0;
+  secondVar = 1;
+
+  return firstVar == secondVar;
+}
+
+function testTrueLooseNonEqualityComparison(){
+  firstVar = 0;
+  secondVar = 1;
+
+  return firstVar != secondVar;
+}
+
+function testTrueLooseEqualityComparison(){
+  firstVar = 1;
+  secondVar = "1";
+
+  return firstVar == secondVar;
+}
+
+function testFalseStrictEqualityComparison(){
+  firstVar = 1;
+  secondVar = "1";
+
+  return firstVar === secondVar;
+}
+
+function testTrueStrictEqualityComparison(){
+  firstVar = 1;
+  secondVar = "1";
+
+  return firstVar === parseInt(secondVar);
+}
