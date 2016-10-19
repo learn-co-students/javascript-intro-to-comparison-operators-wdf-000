08db2d1471ab6473f9df1949ccb4cc77da0c75ee
Flatiron School <github email address>
2016-10-19 17:15:13 -0400

    done

--- js/comparison.js
+++ js/comparison.js
@@ -1,1 +1,34 @@
 'use strict';
+
+function testFalseStrictEqualityComparison() {
+  var zero = 0
+  var one = 1
+  return zero == one
+}
+
+
+function testTrueStrictEqualityComparison() {
+  var one = 1
+  var one_string = "1"
+  return one !== one_string
+}
+
+function testFalseLooseEqualityComparison() {
+  var zero = 0
+  var one = 1
+  return zero == one
+}
+
+
+function testTrueLooseNonEqualityComparison() {
+  var zero = 0
+  var one = 1
+  return zero != one
+}
+
+
+function testTrueLooseEqualityComparison() {
+  var one = 1
+  var one_string = "1"
+  return one == one_string
+}
