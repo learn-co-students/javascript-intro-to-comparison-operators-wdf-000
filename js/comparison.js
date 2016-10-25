b83e891acfb2b02adf35d105b18435cbdcfae976
sami4thewin <samiomarmohammed@gmail.com>
2016-10-25 11:29:21 -0400

    Done.

--- js/comparison.js
+++ js/comparison.js
@@ -1,1 +1,32 @@
 'use strict';
+
+function testFalseLooseEqualityComparison() {
+  var zero = 0;
+  var one = 1;
+  return zero == one;
+}
+
+function testTrueLooseNonEqualityComparison() {
+  var zero = 0;
+  var one = 1;
+  return zero != one;
+}
+
+function testTrueLooseEqualityComparison() {
+  var shwam = 1;
+  var one = "1";
+  return shwam == one;
+}
+
+function testFalseStrictEqualityComparison() {
+  var shwam = 1;
+  var one = "1";
+  return shwam === one;
+}
+
+
+function testTrueStrictEqualityComparison() {
+  var shwam = 1;
+  var one = "1";
+  return shwam === parseInt(one);
+}
