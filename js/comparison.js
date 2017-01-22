facf3a734d548c616dee51e82b17ace9cabf5e44
Ekaterina Petushova <epetushova@gmail.com>
2017-01-22 15:43:09 -0500

    Done.

--- js/comparison.js
+++ js/comparison.js
@@ -1,1 +1,32 @@
 'use strict';
+
+function testFalseLooseEqualityComparison(){
+	var zero = 0;
+	var bar = 1;
+	return zero == bar
+}
+
+function testTrueLooseNonEqualityComparison(){
+	var zero = 0;
+	var bar = 1;
+	return zero != bar
+}
+
+function testTrueLooseEqualityComparison(){
+	var bar = 1;
+	var string = "1";
+	return bar == string
+}
+
+function testFalseStrictEqualityComparison(){
+	var bar = 1;
+	var string = "1";
+	return bar === string
+}
+
+function testTrueStrictEqualityComparison(){
+	var bar = 1;
+	var string = "1";
+	return bar === parseInt(string)
+}
+
