ac035ac720d02246458646aa812fc0c586e80fe9
dimitriwashere <dimitriwashere@gmail.com>
2017-01-30 18:05:35 -0500

    Done.

--- js/comparison.js
+++ js/comparison.js
@@ -1,1 +1,31 @@
 'use strict';
+
+function testFalseLooseEqualityComparison() {
+  var zero = 0;
+  var one = 1;
+  return zero == one;
+}
+
+function testTrueLooseNonEqualityComparison() {
+  var zero = 0;
+  var one = 1;
+  return zero != one;
+}
+
+function testTrueLooseEqualityComparison() {
+  var one = 1;
+  var two = "1";
+  return one == two;
+}
+
+function testFalseStrictEqualityComparison() {
+  var one = 1;
+  var two = "1";
+  return one === two;
+}
+
+function testTrueStrictEqualityComparison() {
+  var one = 1;
+  var two = "1";
+  return one == two;
+}
